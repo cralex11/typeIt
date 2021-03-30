70bce22291757a6881bd25a369aca4582a3039b7
cralex11 <cristeaalex7@gmail.com>
2021-03-30 15:14:34 +0300

    dsds

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -5,7 +5,7 @@ const AuthPage = () => {
     return (
         <div className="row">
             <div className="col s6 offset-s3">
-                <h1>typeIt</h1>
+                <h1>typeIt Logo</h1>
                 <div className="card blue-grey darken-1">
                     <div className="card-content white-text">
                         <span className="card-title">Card Title</span>
