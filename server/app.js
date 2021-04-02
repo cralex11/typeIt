e1d13da82a13da026acf4498756329cc9ef46355
cralex11 <cristeaalex7@gmail.com>
2021-04-02 13:54:27 +0300

    Changes:   - project have been reorganized   - all work

---
+++ server/app.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+//middleware
+
+app.use(cors());
+app.use(express.json({ extended: true }));
+
+//db connection
+async function start() {
+  try {
+    await mongoose.connect(process.env.DB_CONNECTION, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+    });
+  } catch (e) {
+    console.log("Server Error", e.message);
+    process.exit();
+  }
+}
+
+start().then(() => console.log("db started"));
+//--------------------------
+//test
+app.post("/", (req, res) => {
+  res.send("good");
+});
+
+//use routes
+app.use("/api/auth", require("./routes/auth.routes"));
+
+app.listen(process.env.PORT || 5000, () =>
+  console.log(`App has been started on port ${process.env.PORT || 5000}`)
+);
